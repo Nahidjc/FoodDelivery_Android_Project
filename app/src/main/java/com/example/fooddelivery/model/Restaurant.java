c0e3d9381bdbed068f83c1988745637a7f512af6
Nahid2194 <nahidjc80@gmail.com>
2021-04-02 08:46:29 +0600

    model adding

---
+++ app/src/main/java/com/example/fooddelivery/model/Restaurant.java
@@ -0,0 +1,54 @@
+package com.example.fooddelivery.model;
+
+import java.util.List;
+
+public class Restaurant {
+    String restaurantName;
+    String restaurantDescription;
+    String restaurantImgUrl;
+    String restaurantLocation;
+    List<MenuItem> restaurantMenuList;
+
+    public Restaurant() {
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getRestaurantDescription() {
+        return restaurantDescription;
+    }
+
+    public void setRestaurantDescription(String restaurantDescription) {
+        this.restaurantDescription = restaurantDescription;
+    }
+
+    public String getRestaurantImgUrl() {
+        return restaurantImgUrl;
+    }
+
+    public void setRestaurantImgUrl(String restaurantImgUrl) {
+        this.restaurantImgUrl = restaurantImgUrl;
+    }
+
+    public String getRestaurantLocation() {
+        return restaurantLocation;
+    }
+
+    public void setRestaurantLocation(String restaurantLocation) {
+        this.restaurantLocation = restaurantLocation;
+    }
+
+    public List<MenuItem> getRestaurantMenuList() {
+        return restaurantMenuList;
+    }
+
+    public void setRestaurantMenuList(List<MenuItem> restaurantMenuList) {
+        this.restaurantMenuList = restaurantMenuList;
+    }
+}
