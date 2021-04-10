4c70bea45e471bf5e9b7d01f363d1cd1a4d56f92
Nahid2194 <nahidjc80@gmail.com>
2021-04-10 13:24:00 +0600

    updated

--- app/src/main/java/com/example/fooddelivery/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/fooddelivery/ui/dashboard/DashboardFragment.java
@@ -1,9 +1,11 @@
 package com.example.fooddelivery.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,6 +15,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.fooddelivery.LoginActivity;
 import com.example.fooddelivery.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -23,11 +26,22 @@ public class DashboardFragment extends Fragment {
   FirebaseUser user;
   ImageView userprofile;
   TextView emailText,nameText;
+  FirebaseAuth mAuth;
+  Button SignOut;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        mAuth = FirebaseAuth.getInstance();
+        SignOut = root.findViewById(R.id.signout);
+        SignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                signOutUser();
+            }
+        });
 
         userprofile = root.findViewById(R.id.imageView);
         emailText = root.findViewById(R.id.emailID);
@@ -39,5 +53,10 @@ public class DashboardFragment extends Fragment {
             Picasso.get().load(user.getPhotoUrl()).fit().into(userprofile);
         }
         return root;
+    }
+
+    private void signOutUser() {
+        Intent loginActivity = new Intent(this.getActivity(), LoginActivity.class);
+        startActivity(loginActivity);
     }
 }
